bea9fbf57aee331ff06fbb0272a3701f67871a11
Sunday <phamhoangson99@gmail.com>
2025-10-30 12:03:24 +0700

    fix: update cookie domain for production environment

--- services/cookies.ts
+++ services/cookies.ts
@@ -2,7 +2,7 @@ import Cookies from 'universal-cookie'
 
 const cookies = new Cookies(null, {
   path: '/',
-  domain: process.env.NEXT_PUBLIC_ENVIRONMENT === 'local' ? 'localhost' : '.novagate.studio',
+  domain: process.env.NEXT_PUBLIC_ENVIRONMENT === 'local' ? 'localhost' : '.novagate.vn',
 })
 
 export const cookiesInstance = cookies
